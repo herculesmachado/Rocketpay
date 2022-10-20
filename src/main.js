850b2914dbe11f823b1dc6053b105a1261beb9d4
Hércules Machado <herculesprogramador911@gmail.com>
2022-10-20 00:27:57 -0300

    bug na aplicação de troca de cor dos cartões (setCardType)

--- src/main.js
+++ src/main.js
@@ -5,17 +5,15 @@ import IMask from "imask"
 const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
 const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
 
-
 // Pegando a img dos cartões
 const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img')
 
-
-function setcardType(type) {
+function setCardType(type) {
     const colors = {
-        visa: ['#2D57F2', '#436D99'],
-        mastercard: ['#DF6F29', '#C69347'],
-        default: ['back', 'gray'],   
-    }    
+        visa: ["#436D99", "#2D57F2"],
+        mastercard: ["#DF6F29", "#C69347"],
+        default: ["black", "gray"],
+    }  
     
     // Chamando as cores
     ccBgColor01.setAttribute('fill', colors[type][0]),
@@ -25,7 +23,7 @@ function setcardType(type) {
     ccLogo.setAttribute('src', `cc-${type}.svg`)
 }
 
-globalThis.setcardType = setcardType
+globalThis.setCardType = setCardType
 
 // Security Code
 const securityCode = document.querySelector('#security-code')
@@ -66,16 +64,16 @@ const cardNumberPattern = {
         {
             mask: '0000 0000 0000 0000',
             regex: /^4\d{0,15}/,
-            cardType: 'Visa'
+            cardtype: 'Visa'
         },
         {
             mask: '0000 0000 0000 0000',
             regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
-            cardType: 'Mastercard'
+            cardtype: 'Mastercard'
         },
         {
             mask: '0000 0000 0000 0000',
-            cardType: 'Default'
+            cardtype: 'Default'
         },
     ],
     dispatch: function (appended, dynamicMasked) {
@@ -88,13 +86,69 @@ const cardNumberPattern = {
             // Apenas pegando o item do regex
         })
 
-        console.log(foundMask)
+        // setCardType(foundMask.cardtype)
 
         return foundMask
     }
 }
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+// Adicionando o button
+const addButton = document.querySelector('#add-card')
+addButton.addEventListener('click', () => {
+    alert('cartão adicionado')
+})
+
+// Este pedaçõ de codigo, tem como evento, o STOP do reload da pagina
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+})
+
+// Este bloco de codico tem como função pegar os eventos de input do 'nome do titular', todo conteudo novo que eu digite no input, ele vai alterar no cardholder
+const cardHolder = document.querySelector('#card-holder')
+cardHolder.addEventListener('input', () => {
+    const ccHolder = document.querySelector('.cc-holder .value')
+
+    // Estou atribuindo um valor ao meu input, e caso seja true, ele escreve o nome digitado no input, caso seja false, vai aparecer o nome "FULANO DA SILVA" porque vai esta menor que 0
+    ccHolder.innerText = cardHolder.value.length > 0 ? cardHolder.value : 'FULANO DA SILVA'
+})
+
+// OBS: A forma que esta sendo usada a function abaixo, é diferente, mas faz a mesma coisa da de cima, apenas exemplos diferentes com a mesma funcionalidade
+
+// Estes blocos tem como função pegar o que esta sendo digitado no input do CVC, ou seja, a function updateSecurityCode esta fazendo a logica de, pegar o elemento do input pelo querySelector e atribuindo o no innerText. Já o securityCodeMasked esta chamando a function.
+securityCodeMasked.on('accept', () => {
+    updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code){
+    const ccSecurity = document.querySelector('.cc-security .value')
+
+    ccSecurity.innerText = code.length > 0 ? securityCodeMasked : '1234'
+}
+
+// Pegando o numero do cartão atraves dos mesmo exemplos.
+// Este formato faz a mesmo coisa, apenas achei mais facil de escrever e entende a logica.
+cardNumberMasked.on('accept', () => {
+
+    // Estou acessando dentro do cardNumberMasked o mask e dentro do o currentMask escolher a melhor opção que se encaixa no tipo do cartão
+    const cardType = cardNumberMasked.masked.currentMask.cardtype[1]
+    
+    setCardType(cardType)
+
+    updateCardNumber(cardNumberMasked.value)
+
+})
+
+function updateCardNumber (number) {
+    const ccNumber = document.querySelector('.cc-number')
+
+    ccNumber.innerText = number.length > 0 ? number : ''
+}
+
+
+
+
 
 
 
